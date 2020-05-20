948a6c2a1677f52c581507eaad3cb8abbc261f3b
Lairard <63643981+Lairard@users.noreply.github.com>
2020-05-20 20:55:13 +0700

    Create Dog Years.js

---
+++ Javascript/Dog Years.js
@@ -0,0 +1,22 @@
+//Define my age
+let myAge = 21;
+
+//Define earlyyear
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+//Substract 2 from myAge
+let laterYears = myAge - 2;
+
+//Multiply laterYears by 4
+laterYears = laterYears * 4;
+
+console.log(`Early years: ${earlyYears} while later years: ${laterYears}.`);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+//Convert my name to lower case
+let myName = 'Coweg'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
